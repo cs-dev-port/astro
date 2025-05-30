fc515772534099cafed1bd9b8e7a5227c7341e24
soothsoothes <125677657+soothsoothes@users.noreply.github.com>
2025-05-29 20:24:13 -0400

    responsive fix

--- app/zodiac/[sign]/page.tsx
+++ app/zodiac/[sign]/page.tsx
@@ -6,7 +6,7 @@ export default function page() {
     <>
       <div className=" bg-gradient-to-b from-blue-500 to-blue-950">
         <Nav />
-        <div className="flex flex-col items-center justify-center h-screen text-white text-center">
+        <div className="flex flex-col items-center justify-center h-screen text-white text-center px-4">
           <ZodiacData />
         </div>
       </div>
